343f3c332e5f17888c0ee3039f23d1c94269dec1
Matilda Mared <matildamared@live.se>
2022-10-31 13:45:35 +0100

    complete advanced-react-hooks exercise 01 stretch goal 04

--- advanced-react-hooks/src/exercise/01.js
+++ advanced-react-hooks/src/exercise/01.js
@@ -3,23 +3,25 @@
 
 import * as React from 'react'
 
-function countReducer(state, newState) {
-  if (typeof newState === 'function') {
-    return {...state, ...newState(state)}
-  } else {
-    return {...state, ...newState}
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {count: state.count + action.step}
+    }
+    default: {
+      throw new Error(`Unsupported action type: ${action.type}`)
+    }
   }
 }
 
-function Counter({initialCount = 0, step = 2}) {
-  const [state, setState] = React.useReducer(countReducer, {
+function Counter({initialCount = 0, step = 4}) {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
 
   const {count} = state
 
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
