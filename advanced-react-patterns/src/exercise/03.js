fd4f632647061b7767b2762c436217ef0364a6c4
Matilda Mared <matildamared@live.se>
2022-11-01 15:30:13 +0100

    restore code

--- advanced-react-patterns/src/exercise/03.js
+++ advanced-react-patterns/src/exercise/03.js
@@ -43,18 +43,17 @@ function ToggleButton(props) {
 }
 
 function App() {
-  // return (
-  //   <div>
-  //     <Toggle>
-  //       <ToggleOn>The button is on</ToggleOn>
-  //       <ToggleOff>The button is off</ToggleOff>
-  //       <div>
-  //         <ToggleButton />
-  //       </div>
-  //     </Toggle>
-  //   </div>
-  // )
-  return <ToggleButton />
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <div>
+          <ToggleButton />
+        </div>
+      </Toggle>
+    </div>
+  )
 }
 
 export default App
